cde817d5ed939ee1d8b79950b0dc4865ce3453c4
CR Sanchez <americanthinker@gmail.com>
2017-04-05 22:58:57 -0700

    Removed actual credentials

--- Ex2/Twittercredentials.py
+++ Ex2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "9AgE4wWA6W7c2cubVeudk5vw0";
+consumer_key = "Get your own consumer key!";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "6TTM1a7gOFohzsDQabCrL01lytm46xoDiqrHgfzB4tHh6BCALA";
+consumer_secret = "Get your own consumer secret!";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "842028477192658944-8ZR2O9LCUVuQSWBlmp03zxa4C6YjG16";
+access_token = "Get your own access token!!!";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "C5cQdSZc9ZynpWaxXl2Vlc1YlYVKQBkeY2Vnq29jTQVTu";
+access_token_secret = "Get your own secret access token!";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
